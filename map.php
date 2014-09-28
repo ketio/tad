5d470134b6e447b36d1973d95e287357af8f05cb
Geng Yu <liveman24224@gmail.com>
2014-09-28 23:57:11 +0800

    mappp

--- map.php
+++ map.php
@@ -15,6 +15,27 @@
 		?>
 		
 			<div id="map">
+				<div id="marker_1" class="marker">1</div>
+				<div id="marker_2" class="marker">2</div>
+				<div id="marker_3" class="marker">3</div>
+				<div id="marker_4" class="marker">4</div>
+				<div id="marker_5" class="marker">5</div>
+				<div id="marker_6" class="marker">6</div>
+				<div id="marker_7" class="marker">7</div>
+				<div id="marker_8" class="marker">8</div>
+				<div id="marker_9" class="marker">9</div>
+				<div id="marker_10" class="marker">10</div>
+				<div id="marker_11" class="marker">11</div>
+				<div id="marker_12" class="marker">12</div>
+				<div id="marker_13" class="marker">13</div>
+				<div id="marker_14" class="marker">14</div>
+				<div id="marker_15" class="marker">15</div>
+				<div id="marker_16" class="marker">16</div>
+				<!--<div id="marker_17" class="marker">17</div>-->
+				<div id="marker_18" class="marker">18</div>
+				<div id="marker_19" class="marker">19</div>
+				<div id="marker_20" class="marker">20</div>
+				<!--<div id="marker_21" class="marker">21</div>-->
 			</div>
 			<div id="info">
 			</div>
