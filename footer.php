faef8f177446b08758e21554895d1d45175b747e
SteffYang <wonderwallfrank@gmail.com>
2014-09-30 02:05:16 +0800

    adjust

--- footer.php
+++ footer.php
@@ -1,10 +1,16 @@
 <!-- unit introduction -->
 <div>
 	<div class="units">
-		主辦單位	<br>
+		主辦單位
+		<div>
+			<a href="http://www.culture.gov.tw"><img src="img/footer/taipeiculture_gray.png" width="250"></a>
+		</div>
 	</div>
 	<div class="units">
-		執行單位	<br>	
+		執行單位
+		<div>
+			<a href="http://www.aga.org.tw"><img src="img/footer/taiwanatrgallery_gray.png" width="250"></a>
+		</div>	
 	</div>
 	<div id="media">
 		媒體合作	<br>
