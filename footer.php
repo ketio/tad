2db74c6eda67fc97dea984fced26cde14ca91458
Geng Yu <liveman24224@gmail.com>
2014-10-02 00:32:28 +0800

    = =

--- footer.php
+++ footer.php
@@ -1,26 +1,39 @@
 <link type="text/css" rel="stylesheet" href="css/footer_style.css"/>
 <!-- unit introduction -->
-<div>
-	<div class="unit_title">
-		<div>
-			主辦單位
-		</div>
-		<div>
-			執行單位
-		</div>
-	</div>
-	<div class="unit_graph">
-		<div>
-			<a href="http://www.culture.gov.tw"><img src="img/footer/taipeiculture_gray.png" width="250"></a>
-		</div>
-		<div>
-			<a href="http://www.aga.org.tw"><img src="img/footer/taiwanatrgallery_gray.png" width="250"></a>
-		</div>	
-	</div>
-	
-	<div id="media">
-		<div id="media_title">
-			媒體合作
-		</div>
-	</div>				
-</div>
+<table>
+	<tr>
+		<td>
+			<div>
+				主辦單位
+			</div>
+		</td>
+		<td>
+			<div>
+				執行單位
+			</div>
+		</td>
+		<td>
+			<div>
+				媒體合作
+			</div>		
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<div>
+				<a href="http://www.culture.gov.tw"><img src="img/footer/taipeiculture_gray.png" width="250"></a>
+			</div>
+			<div>
+				<a href="http://www.culture.gov.tw"><img src="img/footer/taipeiart_gray.png" width="250"></a>
+			</div>
+		</td>
+		<td>
+			<div>
+				<a href="http://www.aga.org.tw"><img src="img/footer/taiwanatrgallery_gray.png" width="250"></a>
+			</div>	
+		</td>
+		<td>
+			
+		</td>		
+	</tr>
+</table>
