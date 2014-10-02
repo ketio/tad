364b9935cc58d40638ca3bb4cb71e66145bb4841
SteffYang <wonderwallfrank@gmail.com>
2014-10-02 16:58:58 +0800

    guide

--- guide.php
+++ guide.php
@@ -23,8 +23,9 @@
 				</div>
 				<div class="guide_content">
 					聽過、看過、想過卻總是臨門差一腳？那你絕對不能錯過這次大內藝術節的精彩導覽！
-					<br>
+					<div>
 					常有人說，看到畫廊會不敢走進來，大內藝術節將藉由16場畫廊新展、3條風格迥然不同的導覽路線以及熱情的志工，縮短大家與藝術品的距離，等你一起頂著輕鬆的心四處遊走，在街頭巷弄間尋「畫」湊熱鬧！遇見你所不知道的大直內湖藝術區。
+					</div>
 				</div>
 				<div class="guide_category">
 					導覽時間
@@ -38,8 +39,13 @@
 				<div class="guide_name">
 					當代印象
 				</div>
-				<div class="guide_content">
+				<div class="guide_info">
 					 一起穿梭藝品輝煌的從前以及未來，並駐足觀賞「此時此刻」 – 屬於我們年代的進步與活力。 
+				</div>
+				<div class="guide_date">
+					10／25
+					<br>
+					（六）
 				</div>
 			</div>
 			
