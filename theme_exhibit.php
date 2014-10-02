07a911486c8d7435f738a0c80bbb213d28d08d9d
SteffYang <wonderwallfrank@gmail.com>
2014-10-02 15:16:30 +0800

    theme

--- theme_exhibit.php
+++ theme_exhibit.php
@@ -30,9 +30,17 @@
 				<div class="theme_category">
 					展覽時間
 				</div>
+				<div class="theme_content">
+					１０／２４ − １１／１６
+				</div>
 				<div class="theme_category">
 					展覽地點
 				</div>
+				<div class="theme_content">
+					大直中山 21 號廣場（北安路與敬業三路間）
+					<a href="">觀看地圖</a>
+				</div>
+
 				<div class="theme_category">
 					子主題
 				</div>
@@ -41,18 +49,49 @@
 				</div>
 				<div class="arthur">
 					<div class="arthur_photo">
-						<img src="img/themeexhibit/josef-personal(城市精神).jpg" height="275">
+						<img src="img/themeexhibit/josef-personal(城市精神).jpg" height="280">
 					</div>
 					<div class="arthur_info">
+						<strong>藝術家介紹｜Josef Bares</strong>
+						<br>
 						Josef Bares是一名捷克/德國血統的媒體藝術家，現於香港工作生活。他的藝術作品在各大洲展覽過，包括德國、奧地利、俄羅斯和美國等。符號系統領域是他的興趣所在。他用不同的方式去直觀地傳達正在發生意義的過程──意義的創造，尤其熱衷的是城市和語言、空間和語義之間的關係：我們如何「閱讀」我們的城市居住環境。建築在其「本質」中是共通的，但它在文化上卻大相徑庭──這點無異於語言。
 						<br>
- 						台北在城市發展途徑和由此產生的城市形態上，提供了一個和歐洲完全不同的城市經驗。Josef Bares將在更廣闊的「閱讀」城市環境架構中，研究關於運動和記憶的話題。他會尋找連結著運動的場所，可以是橋樑、街道或者公共廣場。上班族通勤途中常見的典型交通路線和場景，會是研究的一個重要方面。這些地點的圖象和印象將會一起在混合媒體藝術作品的形式中起伏呈現。
+ 						台北在城市發展途徑和由此產生的城市形態上，提供了一個和歐洲完全不同的城市經驗。Josef Bares 將在更廣闊的「閱讀」城市環境架構中，研究關於運動和記憶的話題。他會尋找連結著運動的場所，可以是橋樑、街道或者公共廣場。上班族通勤途中常見的典型交通路線和場景，會是研究的一個重要方面。這些地點的圖象和印象將會一起在混合媒體藝術作品的形式中起伏呈現。
 					</div>
 				</div>
 
 				<div class="theme_name">
 					過於喧囂的孤獨：曹良賓、陳以軒
 				</div>
+				<div class="arthur">
+					<div class="arthur_photo">
+						<img src="img/themeexhibit/陳以軒(城市精神).jpg" height="280">
+					</div>
+					<div class="arthur_info">
+						<strong>藝術家介紹｜陳以軒</strong>
+						<br>
+						陳以軒畢業於輔仁大學廣告傳播學系，後於紐約普瑞特藝術學院獲得攝影創作碩士，以臺北和紐約兩個城市作爲創作基地，影像為主要創作媒材。他是唯一一位受邀參加2012年紐約攝影藝術節（New York Photo Festival 2012）的華裔藝術家，並於同年獲選爲 PHOTO TAIPEI 新銳藝術家。
+						<br>
+						2013 年 7 月於 hpgrp GALLERY NEW YORK 舉辦個展《我要離開這個城市了（I am Leaving this City）》，探索在兩個城市之間往返喪失的歸屬感。2014年2月於關渡美術館舉行《你是懂了嘛（Nessun Dorma）》個展，其中主要的錄像作品中陳以軒為義大利歌劇詠嘆調〈公主徹夜未眠〉配上音譯中文字幕，成為無意義中帶有意義的戲謔獨白，呈現台上台下盲目因襲的崇拜。其作品《遍尋無處（Nowhere in Taiwan）》系列尋找自我與土地的認同，於2012年起在紐約展出，後被收爲休士頓美術館（Museum of Fine Arts, Houston）的永久館藏，並將於 2015 年在臺北市立美術館展出。
+					</div>
+				</div>
+
+				<div class="theme_name">
+					小世界，大視界：梁以妮、楊登棋
+				</div>
+				<div class="arthur">
+					<div class="arthur_photo">
+						<img src="img/themeexhibit/楊登棋(城市精神).jpg" height="275">
+					</div>
+					<div class="arthur_info">
+						<strong>藝術家介紹｜楊登棋</strong>
+						<br>
+						自由攝影師與影像美術；1986 年生於台中縣東勢鎮，目前工作與生活在台北，2008 年畢業于大葉大學視覺傳達設計系，實驗影像作品「Marrow 無憂」。
+						<br>
+						獲台北電影節非劇情類特別奬，2009 年從事電影《一頁台北》，《艋舺》美術，2011 年 首次登曼波台北個展 「Sleep Lesson」，2012 年帶著阿嬤赴京都旅行，拍攝第二部個人作品，2013 年擔任林宥嘉 「我們從未不認識 we’ve met before」攝影師，在柏林旅遊與當地藝術家生活，2014 年三月將在上海老碼頭第二個展 「 Im/permanence 無常 」。
+					</div>
+				</div>
+
 
 					
 				
