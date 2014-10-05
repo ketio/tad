7b47fa48b1f1f0e65a35e0b714efb49a03bf27f9
SteffYang <wonderwallfrank@gmail.com>
2014-10-05 22:19:23 +0800

    index

--- index.php
+++ index.php
@@ -214,7 +214,7 @@
 								來過台灣的遊客，都感同身受：台灣最美的風景是「人」。而遊歷台北市之後，無不讚嘆台北都會散發的人文氣質、整潔有序、以及美食的多樣性。
 							</div>
 							<div class="news_more">
-								<a href="/news/2014-7-30.php"> more 〉</a>
+								<a href="http://artemperor.tw/news/5318"> more 〉</a>
 							</div>
 					</div>
 					
@@ -231,7 +231,7 @@
 								「打出「藝術造街」構想，中華民國畫廊協會推動「台北大內藝術區」，昨（30 日）宣告成立，匯集台北市大直、內湖地區的畫廊與藝術空間，目前共有 18 個單位、21 個空間坐落在這片新興區塊，未來將共推展覽，並於每年台北國際⋯⋯
 						</div>
 						<div class="news_more">
-								<a href="/news/2014-7-31.php"> more 〉</a>
+								<a href="http://www.chinatimes.com/newspapers/20140731000897-260115"> more 〉</a>
 						</div>
 					</div>
 					<div class="news_list">
@@ -247,7 +247,7 @@
 								看好大直與內湖地區廠辦空間大、租金相對便宜，與當地科技業、豪宅戶購藏藝術品的潛力，已有累積超過 20 家的畫廊和藝文空間進駐，全台密度最高，昨（30）日這些業者進一步宣布成立「台北大內藝術區」⋯⋯
 							</div>
 							<div class="news_more">
-								<a href="/news/2014-7-31-2.php"> more 〉</a>
+								<a href="http://www.chinatimes.com/newspapers/20140731000135-260204"> more 〉</a>
 							</div>	
 					</div>
 				</div>
