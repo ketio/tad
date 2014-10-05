44ffeb20ca3c461d310326728db2af2b88b7c7fc
Geng Yu <liveman24224@gmail.com>
2014-10-06 03:00:15 +0800

    i wanna sleep ZZZZZZZZZZ

--- workshop.php
+++ workshop.php
@@ -4,8 +4,6 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<title></title>
 	<link type="text/css" rel="stylesheet" href="css/common.css"/>
-	
-
 	<style>
 		#list{
 			width:300px;
@@ -17,6 +15,13 @@
 			background:black;
 			color:white;
 			display:inline-block;
+			cursor:pointer;
+		}
+		.list_item_chosen{
+			background:white;
+			color:black;
+			display:inline-block;
+			cursor:pointer;	
 		}
 		#content{
 			width:830px;
@@ -44,138 +49,164 @@
 					<table>
 						<tr>
 							<td>
-								<div class="list_item" data="1">【藝術x投資】</div>
+								<div id="list_item_1" class="list_item" data="1">【藝術x投資】</div>
 							</td>
 						</tr>
 						<tr>
 							<td>
-								<div class="list_item" data="2">【藝術x時尚】</div>
+								<div id="list_item_2" class="list_item" data="2">【藝術x時尚】</div>
 							</td>
 						</tr>
 						<tr>
 							<td>
-								<div class="list_item" data="3">【藝術x人生】</div>
+								<div id="list_item_3" class="list_item" data="3">【藝術x人生】</div>
 							</td>
 						</tr>
 						<tr>
 							<td>
-								<div class="list_item" data="4">【藝術x居家空間】</div>
+								<div id="list_item_4" class="list_item" data="4">【藝術x居家空間】</div>
 							</td>
 						</tr>
 						<tr>
 							<td>
-								<div class="list_item" data="5">【藝術x閱讀x生活】</div>
+								<div id="list_item_5" class="list_item" data="5">【藝術x閱讀x生活】</div>
 							</td>
 						</tr>	
 						<tr>
 							<td>
-								<div class="list_item" data="6">【70、80兩岸當代藝術家：在路上】</div>
+								<div id="list_item_6" class="list_item" data="6">【70、80兩岸當代藝術家：在路上】</div>
 							</td>
 						</tr>							
 					</table>
 				</div>
 				<div id="content">
 					<div id="content_page_1" class="content_page" data="1">
-						【藝術x投資】收藏是深度的欣賞，投資是深度的收藏
-						進入藝術典藏有沒有門檻？怎樣培養鑑賞藝術的品味？進入藝術投資有沒有門檻？怎樣在令人眼花撩亂、驚艷不止的藝術世界中培養出個人獨特對於藝術的思考與看法？
-						這場與藝術的微沙龍，將與你分享藝術世界、藝術市場沒有界線的親和力，以「收藏是深度的欣賞；投資是深度的收藏」的信念，破除你對藝術投資與收藏互有衝突的迷思。
-
-						主講人: 陸潔民 (畫廊協會資深顧問) （已有圖片）
-						時間：10/25(六)下午3-4點
-						地點: 秋刀魚藝術中心 (台北市中山區基湖路137號1樓)
-						講者介紹：
-						陸潔民於1989年四月在美國加州舊金山南灣矽谷高科技工業區巧遇北京畫院畫家趙秀煥老師，因而調動他心中對藝術有興趣的老靈魂，從科技領域跨到藝術界，開始了他的跳Tone人生。
-						曾任台灣畫廊協會秘書長，籌辦連續三屆的台北國際藝術博覽會，現任台灣畫廊協會資深顧問、上海藝術博覽會顧問、北京藝術博覽會顧問、北京中央美術學院客座教授、IC之音『藝術A B C』節目主持人< w w w .IC 975.com >並出版「藝術A B C」有聲書，他也是上海泓盛拍賣公司顧問、台灣中誠、金仕發藝術品拍賣公司拍賣官。
-						每年積極地參加世界各地的藝術博覽會和藝術活動，使得他擁有廣闊的視野與縝密的觀察力，國際化的藝術投資、亞洲藝術市場的剖析、拍賣市場觀察、藝術博覽會的發展現況、畫廊的經營模式、藝術家與市場供需等，都是他研究的面向，他也不斷以此為藝術市場穩健發展的目標而努力。
+						<div class="content_title">
+							【藝術x投資】收藏是深度的欣賞，投資是深度的收藏
+						</div>
+						<div class="content_body">
+							進入藝術典藏有沒有門檻？怎樣培養鑑賞藝術的品味？進入藝術投資有沒有門檻？怎樣在令人眼花撩亂、驚艷不止的藝術世界中培養出個人獨特對於藝術的思考與看法？
+							</br>
+							這場與藝術的微沙龍，將與你分享藝術世界、藝術市場沒有界線的親和力，以「收藏是深度的欣賞；投資是深度的收藏」的信念，破除你對藝術投資與收藏互有衝突的迷思。
+							</br>
+							主講人: 陸潔民 (畫廊協會資深顧問) （已有圖片）</br>
+							時間：10/25(六)下午3-4點</br>
+							地點: 秋刀魚藝術中心 (台北市中山區基湖路137號1樓)</br>
+							講者介紹：</br>
+							陸潔民於1989年四月在美國加州舊金山南灣矽谷高科技工業區巧遇北京畫院畫家趙秀煥老師，因而調動他心中對藝術有興趣的老靈魂，從科技領域跨到藝術界，開始了他的跳Tone人生。
+							</br>
+							曾任台灣畫廊協會秘書長，籌辦連續三屆的台北國際藝術博覽會，現任台灣畫廊協會資深顧問、上海藝術博覽會顧問、北京藝術博覽會顧問、北京中央美術學院客座教授、IC之音『藝術A B C』節目主持人(www.IC975.com)並出版「藝術A B C」有聲書，他也是上海泓盛拍賣公司顧問、台灣中誠、金仕發藝術品拍賣公司拍賣官。
+							</br>
+							每年積極地參加世界各地的藝術博覽會和藝術活動，使得他擁有廣闊的視野與縝密的觀察力，國際化的藝術投資、亞洲藝術市場的剖析、拍賣市場觀察、藝術博覽會的發展現況、畫廊的經營模式、藝術家與市場供需等，都是他研究的面向，他也不斷以此為藝術市場穩健發展的目標而努力。
+						</div>
 					</div>
 					<div id="content_page_2" class="content_page" data="2">
-						【藝術x時尚】當藝術遇見時尚
-						時尚，是設計師們發揮才華的場域，是身為時尚雜誌編輯工作內容的重點，是潮流男女身上的精彩的打扮。藝術，對多數人來說，是屬於美術館，屬於收藏家...，常是遙不可及！儘管藝術家的創作靈感，經常是來自生活的反芻。一開始，時尚與藝術，看來毫無交集！
-
-						而這場定題為「藝術與時尚跨界實驗」，是反其道而行地邀請藝術家們在令人眼花撩亂的時尚中擷取靈感，透過分析拆解，重新架構他們的創作，把「藝術」轉換成濾光片般的功能，讓觀賞者透過這張濾光片，看到時尚的另一種有趣樣貌。
-
-						主講人:盧淑芬 (Elle她雜誌總編輯)（已有圖片）
-						時間：10/26(日)下午3-4點
-						地點: 學學文創志業 (台北市內湖區堤頂大道二段207號4F)
-						講者介紹：
-						《ELLE國際中文版》總編輯
-						《H arper's B azaar國際中文版》總編輯
-						《Elle國際中文版》時尚總監 13年
-
-						出版創作
-						世界名牌聖經(大雅生活館)
-						名牌高瘋會(大雅生活館)
-						新流行世紀(大雅生活館)
-
+						<div class="content_title">
+							【藝術x時尚】當藝術遇見時尚
+						</div>
+						<div class="content_body">
+							時尚，是設計師們發揮才華的場域，是身為時尚雜誌編輯工作內容的重點，是潮流男女身上的精彩的打扮。藝術，對多數人來說，是屬於美術館，屬於收藏家...，常是遙不可及！儘管藝術家的創作靈感，經常是來自生活的反芻。一開始，時尚與藝術，看來毫無交集！
+							</br>
+							而這場定題為「藝術與時尚跨界實驗」，是反其道而行地邀請藝術家們在令人眼花撩亂的時尚中擷取靈感，透過分析拆解，重新架構他們的創作，把「藝術」轉換成濾光片般的功能，讓觀賞者透過這張濾光片，看到時尚的另一種有趣樣貌。
+							</br>
+							主講人:盧淑芬 (Elle她雜誌總編輯)（已有圖片）</br>
+							時間：10/26(日)下午3-4點</br>
+							地點: 學學文創志業 (台北市內湖區堤頂大道二段207號4F)</br>
+							講者介紹：</br>
+							《ELLE國際中文版》總編輯</br>
+							《H arper's B azaar國際中文版》總編輯</br>
+							《Elle國際中文版》時尚總監 13年</br>
+							</br>
+							出版創作</br>
+							世界名牌聖經(大雅生活館)</br>
+							名牌高瘋會(大雅生活館)</br>
+							新流行世紀(大雅生活館)</br>
+						</div>
 					</div>
 					<div id="content_page_3" class="content_page" data="3">
-						【藝術x人生】如何詩意的活在世上 (演講以英文進行The speech is conducted in English)
-						如何詩意地活在世上？學者兼紅樓夢評論家劉再復認為林黛玉活出了人生最高境界：詩意地活著。因此，她也是唯一了解賈寶玉的人。【藝術 x人生】講座探討藝術和人生的關係，並主張大眾所理解的「用藝術來豐富人生」是對藝術和人生本質的誤解。不論是「用」藝術來達到目的,或是把藝術視為欣賞對像,都會導向自我的無限膨脹，最終反而活出悖離人生的生活。這場演講將以細讀文學和電影作品為主，探討與藝術、品味、美學相關的議題。演講以英語進行，並輔以國語。
-						How to live poetically? This is a question posted by Liu Zai-fu, a scholar and a critic of The Story of the Stone. Liu thinks that Dai-yu represents the highest way of living: to live poetically. That is why she is the only person who understands Pao-yu. The talk explores the role of art in life, arguing that to ‘use’ art to ‘enrich’ life, as people usually say, is a misunderstanding of both the nature of art and life. ‘Using’ art or to ‘appreciate’ art will only lead to a fake life, and eventually living a life which repudiates life itself. The talk will discuss a few literary and filmic texts, exploring issues related to art, taste and aesthetics. The talk will be conducted in English, complemented by limited Mandarin.
-						主講人: 勞維俊 Louis Lo (台北科技大學  英文系文化研究助理教授) （已有圖片）
-						時間：11/09(日) 
-						地點: 亞洲藝術中心 (台北市中山區樂群二路93號)
-						講者介紹：
-						香港大學比較文學哲學博士。喜歡文學、電影、和胡思亂想，前半生誤入工程界，後來以寫作和教學為生，閒來攝影跳舞到處去，好結交怪人。研究興趣：十九世紀以降英美文學、批判理論、性別研究、電影研究、城市與空間理論、中國古典文學。著有:Louis Lo, M ale Jealousy: Literature and Film (C ontinuum , 2008) 和W alking M acao, R eading the B aroque (H K U Press, 2009) (w ith J. Tam bling)。攝影作品可見於近作 ‘B aroque M acao: The C ity, H istory and the D ialectical Im age,’ M acau – C ultural Interaction and Literary R epresentation (R outledge, 2013)。短片導演:《亂語臺北》 (Taipei Astray),2014。
-
-
+						<div class="content_title">		
+							【藝術x人生】如何詩意的活在世上 
+						</div>
+						<div class="content_body">
+							(演講以英文進行The speech is conducted in English)</br>
+							如何詩意地活在世上？學者兼紅樓夢評論家劉再復認為林黛玉活出了人生最高境界：詩意地活著。因此，她也是唯一了解賈寶玉的人。【藝術 x人生】講座探討藝術和人生的關係，並主張大眾所理解的「用藝術來豐富人生」是對藝術和人生本質的誤解。不論是「用」藝術來達到目的,或是把藝術視為欣賞對像,都會導向自我的無限膨脹，最終反而活出悖離人生的生活。這場演講將以細讀文學和電影作品為主，探討與藝術、品味、美學相關的議題。演講以英語進行，並輔以國語。</br>
+							How to live poetically? This is a question posted by Liu Zai-fu, a scholar and a critic of The Story of the Stone. Liu thinks that Dai-yu represents the highest way of living: to live poetically. That is why she is the only person who understands Pao-yu. The talk explores the role of art in life, arguing that to ‘use’ art to ‘enrich’ life, as people usually say, is a misunderstanding of both the nature of art and life. ‘Using’ art or to ‘appreciate’ art will only lead to a fake life, and eventually living a life which repudiates life itself. The talk will discuss a few literary and filmic texts, exploring issues related to art, taste and aesthetics. The talk will be conducted in English, complemented by limited Mandarin.</br>
+							</br>
+							主講人: 勞維俊 Louis Lo (台北科技大學  英文系文化研究助理教授) （已有圖片）</br>
+							時間：11/09(日) </br>
+							地點: 亞洲藝術中心 (台北市中山區樂群二路93號)</br>
+							講者介紹：</br>
+							香港大學比較文學哲學博士。喜歡文學、電影、和胡思亂想，前半生誤入工程界，後來以寫作和教學為生，閒來攝影跳舞到處去，好結交怪人。研究興趣：十九世紀以降英美文學、批判理論、性別研究、電影研究、城市與空間理論、中國古典文學。著有:Louis Lo, M ale Jealousy: Literature and Film (C ontinuum , 2008) 和W alking M acao, R eading the B aroque (H K U Press, 2009) (w ith J. Tam bling)。攝影作品可見於近作 ‘B aroque M acao: The C ity, H istory and the D ialectical Im age,’ M acau – C ultural Interaction and Literary R epresentation (R outledge, 2013)。短片導演:《亂語臺北》 (Taipei Astray),2014。
+						</div>
 					</div>
 					<div id="content_page_4" class="content_page" data="4">
-						【藝術x居家空間】讓當代藝術更貼近生活
-						居住在充滿藝術的居家空間裡，可以說是人人夢寐以求的願望。而其實在空間當中打造屬於自己風格的藝術感營造也並非遙不可及，近年來國內外多場的飯店型博覽會就是趨勢的例子，以這類型的展覽參觀讓民眾更能體會當藝術品成為生活的一部份。
-						現代人講求讓生活過得便利的同時也追求各種美感融入生活，藝術生活融合傢俱以及生活空間到日常的想像當中，居家空間與主人的關係是會相互影響的個性，可以見得現在的當代藝術結合了生活美學的新穎風格，也同時也是讓藝術更貼近生活。
-
-						主講人: 王正凱（一一創意設計有限公司 藝術總監）（已有圖片）
-						時間：11/01(六): 下午3-4點
-						地點: 藝境畫廊 (台北市內湖區瑞光路639號1樓)
-						講者介紹：
-						1964年 生於台北
-						1989年 國立藝術學院(今台北藝術大學)美術系畢業
-						1995年 「泱田影視傳播有限公司」 製片經理 
-						2000年 楊德昌導演 電影『一一』 藝術指導 
-						2003年 「鎧甲娛樂科技股份有限公司」動畫電影製作監督
-						2007年 周美玲導演 電影『漂浪青春』 藝術指導
-						2008 年 晴山國際行銷顧問有限公司 藝術總監
-						2011 年 一一創意設計有公司 藝術總監
-
-						出版創作
-						1986年 發表複合媒體藝術展演『情境/芭蕾』 台北市立美術館
-						1993年 發表個展 台北 「伊通公園」畫廊
-						1993年 舞台劇『非關男女』舞台設計 台北台灣藝術館
-
-
+						<div class="content_title">
+							【藝術x居家空間】讓當代藝術更貼近生活
+						</div>
+						<div class="content_body">						
+							居住在充滿藝術的居家空間裡，可以說是人人夢寐以求的願望。而其實在空間當中打造屬於自己風格的藝術感營造也並非遙不可及，近年來國內外多場的飯店型博覽會就是趨勢的例子，以這類型的展覽參觀讓民眾更能體會當藝術品成為生活的一部份。
+							</br>
+							現代人講求讓生活過得便利的同時也追求各種美感融入生活，藝術生活融合傢俱以及生活空間到日常的想像當中，居家空間與主人的關係是會相互影響的個性，可以見得現在的當代藝術結合了生活美學的新穎風格，也同時也是讓藝術更貼近生活。
+							</br>
+							主講人: 王正凱（一一創意設計有限公司 藝術總監）（已有圖片）</br>
+							時間：11/01(六): 下午3-4點</br>
+							地點: 藝境畫廊 (台北市內湖區瑞光路639號1樓)</br>
+							</br>
+							講者介紹：</br>
+							1964年 生於台北</br>
+							1989年 國立藝術學院(今台北藝術大學)美術系畢業</br>
+							1995年 「泱田影視傳播有限公司」 製片經理 </br>
+							2000年 楊德昌導演 電影『一一』 藝術指導 </br>
+							2003年 「鎧甲娛樂科技股份有限公司」動畫電影製作監督</br>
+							2007年 周美玲導演 電影『漂浪青春』 藝術指導</br>
+							2008 年 晴山國際行銷顧問有限公司 藝術總監</br>
+							2011 年 一一創意設計有公司 藝術總監</br>
+							</br>
+							出版創作</br>
+							1986年 發表複合媒體藝術展演『情境/芭蕾』 台北市立美術館</br>
+							1993年 發表個展 台北 「伊通公園」畫廊</br>
+							1993年 舞台劇『非關男女』舞台設計 台北台灣藝術館</br>
+						</div>
 					</div>
 					<div id="content_page_5" class="content_page" data="5">
-						【藝術x閱讀x 生活】閱讀，打開藝術的對話
-
-						藝術是…人類心靈的收集，歷史的見證，時代的痕跡，這為藝術產生了價值，也產生了距離，只因你讀不懂，看不清，說不出，這就好像聽古典樂，你覺得美好，但卻不懂其意。
-						 
-						其實畫中有話，別怪你讀不懂，因為多數人也是跟你一樣不明其意，以為看不懂就是藝術，但藝術是要讓你用心去讀懂的，所以我們將透過這場分享，與你一起聽見藝術的聲音，與創作者來場心靈對話。
-						 
-						除了，我們將帶你一起來看藝術，也與你一起來創作藝術，一起用心進入奇想的《愛麗絲奇境》，打開你的雙眼，閱讀，無所不在。
-						 
-						讀懂了這個世界，本身就是一門藝術。
-						
-						主講人: 鄭俊德 (華人閱讀社群主編)（已有圖片）
-						時間：11/08(六)
-						地點：餘白 Yuhaku Gallery (台北市中山區敬業二路69巷28號)
-						講者介紹：
-						現為華人閱讀社群主編，總會員社群人數近百萬。
-						演講經歷
-						TED xTaipei open m ic、社企流年會、交點 、中小企業總會、高雄市政府、今周刊、商業週刊、中華文化總會等等。
-
+						<div class="content_title">					
+							【藝術x閱讀x 生活】閱讀，打開藝術的對話
+						</div>
+						<div class="content_body">
+							藝術是…人類心靈的收集，歷史的見證，時代的痕跡，這為藝術產生了價值，也產生了距離，只因你讀不懂，看不清，說不出，這就好像聽古典樂，你覺得美好，但卻不懂其意。
+							</br>
+							其實畫中有話，別怪你讀不懂，因為多數人也是跟你一樣不明其意，以為看不懂就是藝術，但藝術是要讓你用心去讀懂的，所以我們將透過這場分享，與你一起聽見藝術的聲音，與創作者來場心靈對話。
+							</br>
+							除了，我們將帶你一起來看藝術，也與你一起來創作藝術，一起用心進入奇想的《愛麗絲奇境》，打開你的雙眼，閱讀，無所不在。
+							</br>
+							讀懂了這個世界，本身就是一門藝術。
+							</br>
+							</br>
+							主講人: 鄭俊德 (華人閱讀社群主編)（已有圖片）</br>
+							時間：11/08(六)</br>
+							地點：餘白 Yuhaku Gallery (台北市中山區敬業二路69巷28號)</br>
+							講者介紹：</br>
+							現為華人閱讀社群主編，總會員社群人數近百萬。</br>
+							演講經歷</br>
+							TED xTaipei open m ic、社企流年會、交點 、中小企業總會、高雄市政府、今周刊、商業週刊、中華文化總會等等。
+						</div>
 					</div>
 					<div id="content_page_6" class="content_page" data="6">
+						<div class="content_title">	
+							【70、80兩岸當代藝術家：在路上】
+						</div>
+						<div class="content_body">	
 							70後的新世代藝術家，到底是一群要書寫未來歷史的一群？或是被消費的一群？我們該如何看待新世代的藝術家作品？兩岸的新世代藝術家又有怎樣的異同之處呢？新世代藝術家是怎樣出線？又是怎樣離開了跑道？
-
-							主講人: 鄭乃銘 (CANS 藝術新聞總編輯)（已有圖片）
-							時間：11/02(日) 下午3-4點
-							地點:旻谷藝術(台北市內湖區堤頂大道二段295號)
-							講者介紹：
-							曾任自由時報藝文組記者、自由時報藝文組召集人、C A N S藝術新聞雜誌副總編輯。
-							出版創作有《畫布後的天空》、《七年之癢—在藝術與異數之間》、《藝術家看公共藝術》、《裝置的藝術》。
+							</br>
+							</br>
+							主講人: 鄭乃銘 (CANS 藝術新聞總編輯)（已有圖片）</br>
+							時間：11/02(日) 下午3-4點</br>
+							地點:旻谷藝術(台北市內湖區堤頂大道二段295號)</br>
+							講者介紹：</br>
+							曾任自由時報藝文組記者、自由時報藝文組召集人、C A N S藝術新聞雜誌副總編輯。</br>
+							出版創作有《畫布後的天空》、《七年之癢—在藝術與異數之間》、《藝術家看公共藝術》、《裝置的藝術》。</br>
+						</div>
 					</div>					
-					
 				</div>
 			</div>					
 		</div>
@@ -185,6 +216,8 @@
 <script>
 	var now_workshop=0;
 	$(document).ready(function(){
+	
+		show_workshop_content(1);
 		
 		$(".list_item").click(function(){			
 			show_workshop_content($(this).attr("data"));
@@ -192,10 +225,12 @@
 	});
 	function show_workshop_content(workshop_no){
 	
+		$("#list_item_"+now_workshop).removeClass("list_item_chosen");
 		$("#content_page_"+now_workshop).css("visibility","hidden");
 		
 		now_workshop=workshop_no;
 		
+		$("#list_item_"+now_workshop).addClass("list_item_chosen");
 		$("#content_page_"+now_workshop).css("visibility","visible");
 		
 	}
