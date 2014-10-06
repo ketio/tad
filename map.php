9e368aa0ec2781e0a122d2468202db0dc6224b6b
SteffYang <wonderwallfrank@gmail.com>
2014-10-06 22:14:49 +0800

    map

--- map.php
+++ map.php
@@ -4,7 +4,7 @@
 	<link type="text/css" rel="stylesheet" href="css/common.css"/>
 	<link rel="stylesheet" href="css/map.css">	
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title></title>
+	<title>大內藝術節｜TAD 地圖</title>
 	<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
 	<script>
 		var now_gallery="0";
@@ -66,118 +66,118 @@
 					<div id="detail_info">
 						<div id="gallery_detail_info_1" data="1" class="gallery_detail_info">
 							<div class="detail_info_name">采泥藝術 Chini Gallery</div>
-							<div class="detail_info_address">台北市中山區敬業一路128巷46、48號1樓</div>
-							<div class="detail_info_website"><a href="http://www.chinigallery.com" target="_blank">http://www.chinigallery.com</a></div>
+							<div class="detail_info_address">台北市中山區敬業一路 128 巷 46、48 號 1 樓</div>
+							<div class="detail_info_website"><a href="http://www.chinigallery.com" target="_blank">前往官方網站 〉</a></div>
 						</div>
 						<div id="gallery_detail_info_2" data="2" class="gallery_detail_info">
 							<div class="detail_info_name">餘白藝廊 Yohaku Gallery</div>
-							<div class="detail_info_address">台北市中山區敬業二路69巷28號</div>
-							<div class="detail_info_website"><a href="http://www.yohakugallery.com" target="_blank">http://www.yohakugallery.com</a></div>
+							<div class="detail_info_address">台北市中山區敬業二路 69 巷 28 號</div>
+							<div class="detail_info_website"><a href="http://www.yohakugallery.com" target="_blank">前往官方網站 〉</a></div>
 						</div>		
 						<div id="gallery_detail_info_3" data="3" class="gallery_detail_info">
 							<div class="detail_info_name">菲利浦畫廊 Philippe Staib Gallery</div>
-							<div class="detail_info_address">台北市中山區敬業一路128巷38號</div>
-							<div class="detail_info_website"><a href="http://www.philippestaibgallery.com" target="_blank">http://www.philippestaibgallery.com</a></div>
+							<div class="detail_info_address">台北市中山區敬業一路 128 巷 38 號</div>
+							<div class="detail_info_website"><a href="http://www.philippestaibgallery.com" target="_blank">前往官方網站 〉</a></div>
 						</div>		
 						<div id="gallery_detail_info_4" data="4" class="gallery_detail_info">
 							<div class="detail_info_name">青雲畫廊 Cloud Gallery</div>
-							<div class="detail_info_address">台北市中山區明水路469、471號</div>
-							<div class="detail_info_website"><a href="http://www.cloud-gallery.org/" target="_blank">http://www.cloud-gallery.org/</a></div>
+							<div class="detail_info_address">台北市中山區明水路 469、471 號</div>
+							<div class="detail_info_website"><a href="http://www.cloud-gallery.org/" target="_blank">前往官方網站 〉</a></div>
 						</div>		
 						<div id="gallery_detail_info_5" data="5" class="gallery_detail_info">
 							<div class="detail_info_name">亞洲藝術中心 Asia Art Center</div>
-							<div class="detail_info_address">台北市中山區樂群二路93號</div>
-							<div class="detail_info_website"><a href="http://www.asiaartcenter.org" target="_blank">http://www.asiaartcenter.org</a></div>
+							<div class="detail_info_address">台北市中山區樂群二路 93 號</div>
+							<div class="detail_info_website"><a href="http://www.asiaartcenter.org" target="_blank">前往官方網站 〉</a></div>
 						</div>
 						<div id="gallery_detail_info_6" data="6" class="gallery_detail_info">
 							<div class="detail_info_name">藝術計畫 Art Issue Projects</div>
-							<div class="detail_info_address">台北市內湖區堤頂大道二段407巷32號1樓</div>
-							<div class="detail_info_website"><a href="http://www.art-issue.com/" target="_blank">http://www.art-issue.com/</a></div>
+							<div class="detail_info_address">台北市內湖區堤頂大道二段 407 巷 32 號 1 樓</div>
+							<div class="detail_info_website"><a href="http://www.art-issue.com/" target="_blank">前往官方網站 〉</a></div>
 						</div>		
 						<div id="gallery_detail_info_7" data="7" class="gallery_detail_info">
 							<div class="detail_info_name">秋刀魚藝術中心 Fish Art Center</div>
-							<div class="detail_info_address">台北市中山區基湖路137號1樓</div>
-							<div class="detail_info_website"><a href="http://www.fishart.com.tw" target="_blank">http://www.fishart.com.tw</a></div>
+							<div class="detail_info_address">台北市中山區基湖路 137 號 1 樓</div>
+							<div class="detail_info_website"><a href="http://www.fishart.com.tw" target="_blank">前往官方網站 〉</a></div>
 						</div>		
 						<div id="gallery_detail_info_8" data="8" class="gallery_detail_info">
 							<div class="detail_info_name">耿畫廊 Tina Keng Gallery</div>
-							<div class="detail_info_address">台北市內湖區瑞光路548巷15號1樓</div>
-							<div class="detail_info_website"><a href="http://www.tinakenggallery.com" target="_blank">http://www.tinakenggallery.com</a></div>
+							<div class="detail_info_address">台北市內湖區瑞光路 548 巷 15 號 1 樓</div>
+							<div class="detail_info_website"><a href="http://www.tinakenggallery.com" target="_blank">前往官方網站 〉</a></div>
 						</div>		
 						<div id="gallery_detail_info_9" data="9" class="gallery_detail_info">
 							<div class="detail_info_name">TKG+</div>
-							<div class="detail_info_address">台北市內湖區瑞光路548巷15號B1</div>
-							<div class="detail_info_website"><a href="http://www.tinakenggallery/plus" target="_blank">http://www.tinakenggallery/plus</a></div>
+							<div class="detail_info_address">台北市內湖區瑞光路 548 巷 15 號 B1</div>
+							<div class="detail_info_website"><a href="http://www.tinakenggallery/plus" target="_blank">前往官方網站 〉</a></div>
 						</div>
 						<div id="gallery_detail_info_10" data="10" class="gallery_detail_info">
 							<div class="detail_info_name">TKG+ Projects</div>
-							<div class="detail_info_address">台北市內湖區瑞光路548巷15號4樓</div>
-							<div class="detail_info_website"><a href="http://www.tinakenggallery/plus" target="_blank">http://www.tinakenggallery/plus</a></div>
+							<div class="detail_info_address">台北市內湖區瑞光路 548 巷 15 號 4 樓</div>
+							<div class="detail_info_website"><a href="http://www.tinakenggallery/plus" target="_blank">前往官方網站 〉</a></div>
 						</div>		
 						<div id="gallery_detail_info_11" data="11" class="gallery_detail_info">
 							<div class="detail_info_name">旻谷藝術 Ming Art Gallery</div>
-							<div class="detail_info_address">台北市內湖區堤頂大道二段295號</div>
-							<div class="detail_info_website"><a href="http://www.goethe-ming.com" target="_blank">http://www.goethe-ming.com</a></div>
+							<div class="detail_info_address">台北市內湖區堤頂大道二段 295 號</div>
+							<div class="detail_info_website"><a href="http://www.goethe-ming.com" target="_blank">前往官方網站 〉</a></div>
 						</div>		
 						<div id="gallery_detail_info_12" data="12" class="gallery_detail_info">
 							<div class="detail_info_name">威廉藝術沙龍 William Art Salon</div>
-							<div class="detail_info_address">台北市內湖區堤頂大道二段356號</div>
-							<div class="detail_info_website"><a href="http://www.williamartsalon.com/" target="_blank">http://www.williamartsalon.com/</a></div>
+							<div class="detail_info_address">台北市內湖區堤頂大道二段 356 號</div>
+							<div class="detail_info_website"><a href="http://www.williamartsalon.com/" target="_blank">前往官方網站 〉</a></div>
 						</div>		
 						<div id="gallery_detail_info_13" data="13" class="gallery_detail_info">
 							<div class="detail_info_name">學學白色空間 XUE XUE WHITE</div>
-							<div class="detail_info_address">台北市內湖區堤頂大道二段207號7樓</div>
-							<div class="detail_info_website"><a href="http://white.xuexue.tw" target="_blank">http://white.xuexue.tw</a></div>
+							<div class="detail_info_address">台北市內湖區堤頂大道二段 207 號 7 樓</div>
+							<div class="detail_info_website"><a href="http://white.xuexue.tw" target="_blank">前往官方網站 〉</a></div>
 						</div>
 						<div id="gallery_detail_info_14" data="14" class="gallery_detail_info">
 							<div class="detail_info_name">學學原色空間 XUE XUE GREEN</div>
-							<div class="detail_info_address">台北市內湖區堤頂大道二段207號5樓</div>
-							<div class="detail_info_website"><a href="http://green.xuexue.tw" target="_blank">http://green.xuexue.tw</a></div>
+							<div class="detail_info_address">台北市內湖區堤頂大道二段 207 號 5 樓</div>
+							<div class="detail_info_website"><a href="http://green.xuexue.tw" target="_blank">前往官方網站 〉</a></div>
 						</div>		
 						<div id="gallery_detail_info_15" data="15" class="gallery_detail_info">
 							<div class="detail_info_name">愛力根畫廊 Galerie Elegance Taipei</div>
-							<div class="detail_info_address">台北市內湖區瑞光路408號</div>
-							<div class="detail_info_website"><a href="https://www.facebook.com/galerie.elegance?fref=ts" target="_blank">https://www.facebook.com/galerie.elegance?fref=ts</a></div>
+							<div class="detail_info_address">台北市內湖區瑞光路 408 號</div>
+							<div class="detail_info_website"><a href="https://www.facebook.com/galerie.elegance?fref=ts" target="_blank">前往官方網站 〉</a></div>
 						</div>		
 						<div id="gallery_detail_info_16" data="16" class="gallery_detail_info">
 							<div class="detail_info_name">尊彩藝術中心 Liang Gallery</div>
-							<div class="detail_info_address">台北市內湖區瑞光路366號</div>
-							<div class="detail_info_website"><a href="http://www.lianggallery.com/" target="_blank">http://www.lianggallery.com/</a></div>
+							<div class="detail_info_address">台北市內湖區瑞光路 366 號</div>
+							<div class="detail_info_website"><a href="http://www.lianggallery.com/" target="_blank">前往官方網站 〉</a></div>
 						</div>		
 						<div id="gallery_detail_info_17" data="17" class="gallery_detail_info">
 							<div class="detail_info_name">藝星藝術中心 Star Gallery</div>
-							<div class="detail_info_address">台北市內湖區新湖一路128巷15號1樓</div>
-							<div class="detail_info_website"><a href="www.stargallery.tw" target="_blank">www.stargallery.tw</a></div>
+							<div class="detail_info_address">台北市內湖區新湖一路 128 巷 15 號 1 樓</div>
+							<div class="detail_info_website"><a href="www.stargallery.tw" target="_blank">前往官方網站 〉</a></div>
 						</div>
 						<div id="gallery_detail_info_18" data="18" class="gallery_detail_info">
 							<div class="detail_info_name">台灣工銀藝文空間 Industrial Bank of Taiwan Art and Cultural Galleria</div>
-							<div class="detail_info_address">台北市內湖區堤頂大道二段99號1樓</div>
-							<div class="detail_info_website"><a href="http://www.ibtef.org.tw" target="_blank">http://www.ibtef.org.tw</a></div>
+							<div class="detail_info_address">台北市內湖區堤頂大道二段 99 號 1 樓</div>
+							<div class="detail_info_website"><a href="http://www.ibtef.org.tw" target="_blank">前往官方網站 〉</a></div>
 						</div>		
 						<div id="gallery_detail_info_19" data="19" class="gallery_detail_info">
 							<div class="detail_info_name">沁德居藝廊 Chin Der Jyu Gallery</div>
-							<div class="detail_info_address">台北市中山區堤頂大道2段352號</div>
-							<div class="detail_info_website"><a href="http://tw.myblog.yahoo.com/chinderjyugallery" target="_blank">http://tw.myblog.yahoo.com/chinderjyugallery</a></div>
+							<div class="detail_info_address">台北市中山區堤頂大道二段 352 號</div>
+							<div class="detail_info_website"><a href="http://tw.myblog.yahoo.com/chinderjyugallery" target="_blank">前往官方網站 〉</a></div>
 						</div>		
 						<div id="gallery_detail_info_20" data="20" class="gallery_detail_info">
 							<div class="detail_info_name">哥德藝術中心 Goethe Art Center</div>
-							<div class="detail_info_address">台北市內湖區堤頂大道二段293號</div>
-							<div class="detail_info_website"><a href="http://www.goethe-ming.com" target="_blank">http://www.goethe-ming.com</a></div>
+							<div class="detail_info_address">台北市內湖區堤頂大道二段 293 號</div>
+							<div class="detail_info_website"><a href="http://www.goethe-ming.com" target="_blank">前往官方網站 〉</a></div>
 						</div>	
 						<div id="gallery_detail_info_21" data="21" class="gallery_detail_info">
 							<div class="detail_info_name">名典畫廊 Ming Dian Gallery</div>
-							<div class="detail_info_address">台北市內湖區新湖一路128巷15號2樓B005</div>
-							<div class="detail_info_website"><a href="www.mingdianart.com.tw" target="_blank">www.mingdianart.com.tw</a></div>
+							<div class="detail_info_address">台北市內湖區新湖一路 128 巷 15 號 2 樓 B005</div>
+							<div class="detail_info_website"><a href="www.mingdianart.com.tw" target="_blank">前往官方網站 〉</a></div>
 						</div>			
 						<div id="gallery_detail_info_22" data="22" class="gallery_detail_info">
 							<div class="detail_info_name">大德畫廊 Da De Gallery</div>
-							<div class="detail_info_address">台北市堤頂大道二段297號5樓	</div>
-							<div class="detail_info_website"><a href="https://www.facebook.com/dadeartgallery" target="_blank">https://www.facebook.com/dadeartgallery</a></div>
+							<div class="detail_info_address">台北市堤頂大道二段 297 號 5 樓	</div>
+							<div class="detail_info_website"><a href="https://www.facebook.com/dadeartgallery" target="_blank">前往官方網站 〉</a></div>
 						</div>	
 						<div id="gallery_detail_info_23" data="23" class="gallery_detail_info">
-							<div class="detail_info_name">藝境畫廊 ARTDOOR Galler</div>
-							<div class="detail_info_address">??????</div>
-							<div class="detail_info_website"><a href="http://www.artdoor.com.tw" target="_blank">http://www.artdoor.com.tw</a></div>
+							<div class="detail_info_name">藝境畫廊 ARTDOOR Gallery</div>
+							<div class="detail_info_address"></div>
+							<div class="detail_info_website"><a href="http://www.artdoor.com.tw" target="_blank">前往官方網站 〉</a></div>
 						</div>						
 					</div>
 					<div id="gallery">
