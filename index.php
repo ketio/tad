13c7c890a54f7c4667ea41c009d1490adf1fa965
SteffYang <wonderwallfrank@gmail.com>
2014-10-26 02:47:19 +0800

    news

--- index.php
+++ index.php
@@ -202,57 +202,57 @@
 			</div>
 			<div id="content_inteface">
 				<div id="news_content">
+					
 					<div class="news_list">
 						<div>
 							<div class="news_date">
-								2014／07／30
+								2014／10／24
 							</div>
 							<div class="news_press">
-								台北大內藝術區成立
+								【展覽】《人形雕》日本木雕藝術家聯展
+							</div>
+						</div>
+						<div class="news_content">
+								為配合臺北大內藝術區 (TAD) 成立之聯合開幕，青雲畫廊正式推出日本木雕藝術家聯展，由策展人–賴永興策展，「人‧形‧雕」日本木雕藝術家聯展是由前田忠一和吉田敦兩位日籍藝術家和台籍藝術家–賴永興⋯⋯
+						</div>
+						<div class="news_more">
+								<a href="http://www.arttime.com.tw/ArtNews/news_group_cont1.aspx?AGDID=9826" target="_BLANK"> more 〉</a>
+						</div>
+					</div>
+					<div class="news_list">
+						<div>
+							<div class="news_date">
+								2014／10／06
+							</div>
+							<div class="news_press">
+								「親愛的... 我想對你說」重新檢視與藝術的親密關係
 							</div>
 						</div>
 							<div class="news_content">
-								「藝術」－是台北市重要的城市風景
-								<br>
-								來過台灣的遊客，都感同身受：台灣最美的風景是「人」。而遊歷台北市之後，無不讚嘆台北都會散發的人文氣質、整潔有序、以及美食的多樣性。
+								邁入第 21 個年頭，臺北國際藝術博覽會進入一個嶄新的時代，「Art Taipei 2014」將與藏家一起探索藝術的本質，分享對藝術的愛、恨、情、愁，和藝術一起談場戀愛。臺北藝博藝術總監林怡華笑著說⋯⋯
+							</div>
+							<div class="news_more">
+								<a href="http://artouch.com/m_test/investments/story.aspx?aid=2014100618033" target="_BLANK"> more 〉</a>
+							</div>	
+					</div>
+
+					<div class="news_list">
+						<div>
+							<div class="news_date">
+								2014／07／31
+							</div>
+							<div class="news_press">
+								打造大內藝術區 讓畫廊成街景
+							</div>
+						</div>
+							<div class="news_content">
+								「打出「藝術造街」構想，中華民國畫廊協會推動「台北大內藝術區」，昨（30 日）宣告成立，匯集台北市大直、內湖地區的畫廊與藝術空間，目前共有 18 個單位、21 個空間坐落在這片新興區塊，未來將共推展覽，並於每年台北國際⋯⋯
 							</div>
 							<div class="news_more">
 								<a href="http://artemperor.tw/tidbits_page.php?table=news&id=5318?1" target="_BLANK"> more 〉</a>
 							</div>
 					</div>
 					
-					<div class="news_list">
-						<div>
-							<div class="news_date">
-								2014／07／31
-							</div>
-							<div class="news_press">
-								中國時報：打造大內藝術區 讓畫廊成街景
-							</div>
-						</div>
-						<div class="news_content">
-								「打出「藝術造街」構想，中華民國畫廊協會推動「台北大內藝術區」，昨（30 日）宣告成立，匯集台北市大直、內湖地區的畫廊與藝術空間，目前共有 18 個單位、21 個空間坐落在這片新興區塊，未來將共推展覽，並於每年台北國際⋯⋯
-						</div>
-						<div class="news_more">
-								<a href="http://www.chinatimes.com/newspapers/20140731000897-260115" target="_BLANK"> more 〉</a>
-						</div>
-					</div>
-					<div class="news_list">
-						<div>
-							<div class="news_date">
-								2014／07／31
-							</div>
-							<div class="news_press">
-								中國時報：台北大內藝術區成立 畫廊密度居全台之冠
-							</div>
-						</div>
-							<div class="news_content">
-								看好大直與內湖地區廠辦空間大、租金相對便宜，與當地科技業、豪宅戶購藏藝術品的潛力，已有累積超過 20 家的畫廊和藝文空間進駐，全台密度最高，昨（30）日這些業者進一步宣布成立「台北大內藝術區」⋯⋯
-							</div>
-							<div class="news_more">
-								<a href="http://www.chinatimes.com/newspapers/20140731000135-260204" target="_BLANK"> more 〉</a>
-							</div>	
-					</div>
 				</div>
 				<div id="facebook_content">
 					<iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Ftadfestival&amp;width&amp;height=395&amp;colorscheme=light&amp;show_faces=false&amp;header=false&amp;stream=true&amp;show_border=true" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:480px;height:400px;background:white" allowTransparency="true"></iframe>
--- news.php
+++ news.php
@@ -15,25 +15,40 @@
 		?>
 
 		<div id="content_news">
+
 					<div class="list_news">
 						<div>
 							<div class="date_news">
-								2014／07／30
+								2014／10／24
 							</div>
 							<div class="press_news">
-								台北大內藝術區成立
+								藝術網：【展覽】《人形雕》日本木雕藝術家聯展
+							</div>
+						</div>
+						<div class="content_news">
+								為配合臺北大內藝術區 (TAD) 成立之聯合開幕，青雲畫廊正式推出日本木雕藝術家聯展，由策展人–賴永興策展，「人‧形‧雕」日本木雕藝術家聯展是由前田忠一和吉田敦兩位日籍藝術家和台籍藝術家–賴永興⋯⋯
+						</div>
+						<div class="more_news">
+								<a href="http://www.arttime.com.tw/ArtNews/news_group_cont1.aspx?AGDID=9826" target="_BLANK"> more 〉</a>
+						</div>
+					</div>
+					<div class="list_news">
+						<div>
+							<div class="date_news">
+								2014／10／06
+							</div>
+							<div class="press_news">
+								典藏投資：「親愛的... 我想對你說」重新檢視與藝術的親密關係
 							</div>
 						</div>
 							<div class="content_news">
-								「藝術」－是台北市重要的城市風景
-								<br>
-								來過台灣的遊客，都感同身受：台灣最美的風景是「人」。而遊歷台北市之後，無不讚嘆台北都會散發的人文氣質、整潔有序、以及美食的多樣性。
+								邁入第 21 個年頭，臺北國際藝術博覽會進入一個嶄新的時代，「Art Taipei 2014」將與藏家一起探索藝術的本質，分享對藝術的愛、恨、情、愁，和藝術一起談場戀愛。臺北藝博藝術總監林怡華笑著說⋯⋯
 							</div>
 							<div class="more_news">
-								<a href="http://artemperor.tw/tidbits_page.php?table=news&id=5318?1" target="_BLANK"> more 〉</a>
-							</div>
+								<a href="http://artouch.com/m_test/investments/story.aspx?aid=2014100618033" target="_BLANK"> more 〉</a>
+							</div>	
 					</div>
-					
+
 					<div class="list_news">
 						<div>
 							<div class="date_news">
@@ -50,6 +65,7 @@
 								<a href="http://www.chinatimes.com/newspapers/20140731000897-260115" target="_BLANK"> more 〉</a>
 						</div>
 					</div>
+
 					<div class="list_news">
 						<div>
 							<div class="date_news">
@@ -66,7 +82,96 @@
 								<a href="http://www.chinatimes.com/newspapers/20140731000135-260204" target="_BLANK"> more 〉</a>
 							</div>	
 					</div>
-				</div>
+
+					<div class="list_news">
+						<div>
+							<div class="date_news">
+								2014／07／30
+							</div>
+							<div class="press_news">
+								非池中藝術網：台北大內藝術區成立
+							</div>
+						</div>
+							<div class="content_news">
+								「藝術」－是台北市重要的城市風景
+								<br>
+								來過台灣的遊客，都感同身受：台灣最美的風景是「人」。而遊歷台北市之後，無不讚嘆台北都會散發的人文氣質、整潔有序、以及美食的多樣性。
+							</div>
+							<div class="more_news">
+								<a href="http://artemperor.tw/tidbits_page.php?table=news&id=5318?1" target="_BLANK"> more 〉</a>
+							</div>
+					</div>
+
+					<div class="list_news">
+						<div>
+							<div class="date_news">
+								2014／07／30
+							</div>
+							<div class="press_news">
+								今日報：城市中最優美的風景 台北大內藝術區成立
+							</div>
+						</div>
+							<div class="content_news">
+								為了推動「台北也有一個國際矚目的畫廊區」－社團法人中華民國畫廊協會（以下簡稱畫廊協會）積極運作聯結台北市大直和內湖地區的畫廊和藝文空間⋯⋯
+							</div>
+							<div class="more_news">
+								<a href="http://www.todaynews.com.tw/bencandy.php?fid-6-id-7125-page-1.htm" target="_BLANK"> more 〉</a>
+							</div>
+					</div>
+
+					<div class="list_news">
+						<div>
+							<div class="date_news">
+								2014／07／30
+							</div>
+							<div class="press_news">
+								非池中藝術網：大內藝術區成立 畫廊做鄰居
+							</div>
+						</div>
+							<div class="content_news">
+								美國紐約雀兒喜區將工業荒地轉換為藝術聚落，群聚了百餘家畫廊；中國北京798藝術區則從聯合工廠吸引藝術家進駐開始，現在為北京畫廊重鎮⋯⋯
+							</div>
+							<div class="more_news">
+								<a href="http://artemperor.tw/news_page.php?table=focus&id=506?1" target="_BLANK"> more 〉</a>
+							</div>
+					</div>
+					
+					<div class="list_news">
+						<div>
+							<div class="date_news">
+								2014／07／16
+							</div>
+							<div class="press_news">
+								街廓再造，藝術的經濟力
+							</div>
+						</div>
+							<div class="content_news">
+								藝術村或藝術聚落存在的意義為何？如「空場」提供藝術家一個友善的創作環境，大家可以彼此交流，加深對於自己創作的看法，並且舉辦活動幫助藝術家打開視野，而藝術聚落最主要的⋯⋯
+							</div>
+							<div class="more_news">
+								<a href="http://www.artouch.com/artco/story.aspx?aid=2014071616641" target="_BLANK"> more 〉</a>
+							</div>
+					</div>
+
+					<div class="list_news">
+						<div>
+							<div class="date_news">
+								2014／07／16
+							</div>
+							<div class="press_news">
+								名為藝術街廓的現代都會
+							</div>
+						</div>
+							<div class="content_news">
+								從文化的歷史角度，藝術街廓（arts districts）可說是在後工業化時期興起的一種空間觀念，反映著文化群體運用空間資源時的各種詮釋，以及人類在改變地表的行動上所累積的結果。⋯⋯
+							</div>
+							<div class="more_news">
+								<a href="http://www.artouch.com/artco/story.aspx?aid=2014071616068" target="_BLANK"> more 〉</a>
+							</div>
+					</div>
+					
+		</div>
 		
+		<div id="footer"></div>
 	</body>
 </html>
